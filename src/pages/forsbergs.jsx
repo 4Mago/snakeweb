b4972b8628a2ee435cfa20c99f730507b5472d98
4Mago <47850942+4Mago@users.noreply.github.com>
2021-02-27 22:03:05 +0100

    ddd

--- src/pages/forsbergs.jsx
+++ src/pages/forsbergs.jsx
@@ -21,7 +21,7 @@ const Forsbergs = () => {
       <Button onClick={() => setPdf(true)}>jakobs portfolio</Button>
 
       {pdf ? (
-        <Iframe src="/pdf/ta_spranget.pdf" height="100%" width="70%a" />
+        <Iframe src="/pdf/ta_spranget.pdf" height="100%" width="70%d" />
       ) : undefined}
     </Container>
   )
