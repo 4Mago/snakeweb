fab53f7d35e01b4a64acfe212f7ff90640a626f1
4Mago <47850942+4Mago@users.noreply.github.com>
2021-03-22 14:54:04 +0100

    Update Client.js

--- src/Client.js
+++ src/Client.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client"
 
 export default sanityClient({
-  projectId: "kpvsh5rt",
+  projectId: "qv47i2zl",
   dataset: "production",
   token: "",
   useCdn: true,
